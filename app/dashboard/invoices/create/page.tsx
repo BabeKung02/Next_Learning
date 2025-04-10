7c048cc44c221bf8c4150f5ae7fce67a8d8648bc
BabeKung <ntpbabe02@gmail.com>
2025-04-10 16:19:53 +0700

    Update Chapter 16

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,7 +1,8 @@
+export const dynamic = 'force-dynamic';
 import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
- 
+
 export default async function Page() {
   const customers = await fetchCustomers();
  
